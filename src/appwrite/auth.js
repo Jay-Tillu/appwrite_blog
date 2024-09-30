91876a88c62b9056e809b13fbfa7de8d16d4aa51
Jay Tillu <26956655+Jay-Tillu@users.noreply.github.com>
2024-09-30 08:48:07 +0000

    Redux Added

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -2,61 +2,65 @@ import conf from "../conf/conf";
 import { Client, Account, ID } from "appwrite";
 
 export class AuthService {
-    client = new Client();
-    account;
+  client = new Client();
+  account;
 
-    constructor() {
-        this.client
-            .setEndpoint(conf.appwriteUrl)
-            .setProject(conf.appwriteProjectId);
-        this.account = new Account(this.client);
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+
+  // we create a method so we can create wrapper.
+  // Other components don't know whether this method is using Firebase or Appwrite.
+  // Those components will only know that to create an account
+  // we need to call this method.
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+
+      if (userAccount) {
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log("Appwrite service :: getCurrentUser :: Error", error);
     }
 
-    // we create a method so we can create wrapper.
-    // Other components don't know whether this method is using Firebase or Appwrite.
-    // Those components will only know that to create an account 
-    // we need to call this method. 
-    async createAccount({ email, password, name }) {
-        try {
-            const userAccount = await this.account.create(ID.unique(), email, password, name);
+    return null;
+  }
 
-            if (userAccount) {
-                return this.login({ email, password });
-            } else {
-                return userAccount;
-            }
-        } catch (error) {
-            throw error;
-        }
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      console.log("Appwrite Service :: Logout :: Error", error);
     }
-
-    async login({ email, password }) {
-        try {
-            return await this.account.createEmailSession(email, password);
-        } catch (error) {
-            throw error;
-        }
-    }
-
-    async getCurrentUser() {
-        try {
-            return await this.account.get();
-        } catch (error) {
-            console.log("Appwrite service :: getCurrentUser :: Error", error);
-
-        }
-
-        return null;
-    }
-
-    async logout() {
-        try {
-            await this.account.deleteSessions();
-        } catch (error) {
-            console.log("Appwrite Service :: Logout :: Error", error);
-        }
-    }
+  }
 }
 
 const authService = new AuthService();
-export default authService; 
+export default authService;
